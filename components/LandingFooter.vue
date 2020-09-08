744ed1c0d509782955e1c9c3c4ff5a16621d17a6
codebender828 <jbakebwa@163.com>
2020-09-09 02:04:54 +0800

    add links to source in footer

--- components/LandingFooter.vue
+++ components/LandingFooter.vue
@@ -1,9 +1,12 @@
 <template>
   <c-flex justify="center" my="10">
-    Made with <c-link is-external href="https://vue.chakra-ui.com">
-      <c-code ml="3">
-        @chakra-ui/vue
+    Made with <c-link is-external color="miro.400" text-decoration="underline" href="https://vue.chakra-ui.com">
+      <c-code mx="3">
+        @chakra-ui/vue.
       </c-code>
+      View source on <c-link color="miro.400" text-decoration="underline" is-external href="https://github.com/codebender828/miro-landing-chakra">
+        Github
+      </c-link>
     </c-link>
   </c-flex>
 </template>
